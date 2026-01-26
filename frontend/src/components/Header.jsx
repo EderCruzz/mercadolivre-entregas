19bdd2c97e3a1dab662da537a483658c6c144862
Eder Cruz <eder.ales10@gmail.com>
2026-01-26 07:39:23 -0300

    Ajuste fino no CSS do Card

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,18 +4,20 @@ import logo from "../assets/cepe-logo.png";
 export default function Header({ onSync, syncing }) {
   return (
     <header className="app-header">
-      <div className="header-left">
-        <img src={logo} alt="CEPE" className="logo" />
-      </div>
+      <div className="header-content">
+        <div className="header-left">
+          <img src={logo} alt="CEPE" className="logo" />
+        </div>
 
-      <div className="header-right">
-        <button
-          className="sync-button"
-          onClick={onSync}
-          disabled={syncing}
-        >
-          {syncing ? "Sincronizando..." : "Sincronizar"}
-        </button>
+        <div className="header-right">
+          <button
+            className="sync-button"
+            onClick={onSync}
+            disabled={syncing}
+          >
+            {syncing ? "Sincronizando..." : "Sincronizar"}
+          </button>
+        </div>
       </div>
     </header>
   );
