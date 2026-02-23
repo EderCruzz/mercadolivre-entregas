aad44a1148423c4a8e747336440931b3a956dfdb
Eder Cruz <90397436+EderCruzz@users.noreply.github.com>
2026-02-23 10:24:18 -0300

    Update getToken.js

--- backend/src/utils/getToken.js
+++ backend/src/utils/getToken.js
@@ -8,30 +8,19 @@ module.exports = async function getToken() {
     throw new Error("Token não encontrado");
   }
 
-  if (Date.now() < token.expires_at) {
-    return token.access_token;
-  }
-
-  console.log("🔄 Cron: renovando token");
-
   const response = await axios.post(
     "https://api.mercadolibre.com/oauth/token",
     {
       grant_type: "refresh_token",
       client_id: process.env.ML_CLIENT_ID,
       client_secret: process.env.ML_CLIENT_SECRET,
-      refresh_token: token.refresh_token
     },
     { headers: { "Content-Type": "application/json" } }
   );
 
   token.access_token = response.data.access_token;
-  token.refresh_token = response.data.refresh_token;
-  token.expires_at = Date.now() + response.data.expires_in * 1000;
 
   await token.save();
-
-  console.log("✅ Token renovado automaticamente pelo cron");
 
   return token.access_token;
 };
