a391e2ee319e6ba80615cd56f74ac80826e26442
Eder Cruz <90397436+EderCruzz@users.noreply.github.com>
2026-02-25 16:22:23 -0300

    Update api.js
    voltando para o endereço render provisoriamente

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://72.60.141.173:3333",
+  baseURL: "https://mercadolivre-entregas.onrender.com",
 });
 
 export default api;
