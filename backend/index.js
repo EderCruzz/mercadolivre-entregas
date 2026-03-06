8572f432141e884ee12578912e3fcfcce3b75a40
Eder Cruz <eder.ales10@gmail.com>
2026-03-06 10:57:00 -0300

    Ajustando backend para aceitar rota /palavra-chave novo ajuste

--- backend/index.js
+++ backend/index.js
@@ -586,7 +586,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
